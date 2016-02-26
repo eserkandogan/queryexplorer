e99fcd7e3a038ae96c15e02b24fae4a61affff0f
Eser Kandogan <eser@us.ibm.com>
2016-02-25 20:32:13 -0800

    christina code without /data directoru

---
+++ WebContent/scripts/process.js
@@ -0,0 +1,109 @@
+var id=0;
+
+d3.csv("data/qsp1.csv", function(d) {
+	id= id+1;	
+	var toReturn =  {
+		qspid : id,
+	    count : +d.count,
+	    template : d.template,
+	    columnA : parseColumnSemantics( d.columnA, 1),//columnAobject,
+	    columnB : d.columnB//parseColumnSemantics( d.columnB, 2)
+	  };
+	
+	  return toReturn;
+	
+	}, function(data) {
+		
+	  var templateFilter = data[0].template;
+	  
+	  var filteredByTemplate = data.filter(function( obj ) {
+	    return obj.template == templateFilter;
+		});
+	  $("#template").text(JSON.stringify(templateFilter));
+	  $("#template-badge").text(filteredByTemplate.length);
+ 	  //populateTemplate(data);
+	  populateColumnA(filteredByTemplate);
+	  
+	  $('#columnA').on('click', 'li', function() {
+	  	$( "#columnA div" ).removeClass( "alert-success" );
+	      $('#'+$(this).attr('id')+' div').toggleClass( 'alert-success' );
+	  	var colAlabel = $(this).attr('qspColA');
+	  	var templateFilter = JSON.parse($("#template").text());
+	  	var colBdata = data.filter(function( obj ) {
+	  	    return obj.template == templateFilter && obj.columnA.label== colAlabel;
+	  	});
+	  	populateColumnB(colBdata);
+	  });
+	  
+});//end loading data
+
+//Helper functions
+
+function parseColumnSemantics(data, num){
+	var columnObject = [];
+	var objectElements = data.split(/[[\]]{1,2}/);
+	
+	columnObject["label"] = objectElements[0];
+	columnObject["id"]= objectElements[1];
+	columnObject["abstractionLevel"]= objectElements[2];
+	columnObject["columnCount"]= +objectElements[3];
+
+	return columnObject;
+}
+
+function populateTemplate(data){	
+	stroll.bind( '#datadiv ul' );
+	console.log(data.length)
+	var uniqueEntities = _.uniq(data, function (item, key, a) {return item.template;});
+	console.log(uniqueEntities.length)
+	uniqueEntities.sort(function(a, b){
+    	var a1= a.template, b1= b.template;
+    	if(a1 == b1) return 0;
+    	return a1 > b1? 1: -1;
+	});
+	$.each(uniqueEntities, function( index, value) {		
+  		$('#templatescroller').append('<li id="template_'+index+'" qsptemp="'+value.template+'" class="list-group-item">'+
+  		      '<div class="alert " role="alert"> <span class="badge">'+value.count+'</span>'+
+  		      '<span id="columnA" >'+value.template+'</span></div></li>');
+	});	
+}
+
+	
+function populateColumnA(data){	
+	stroll.bind( '#datadiv ul' );
+
+	data.sort(function(a, b){
+    	var a1= a.columnA.columnCount, b1= b.columnA.columnCount;
+    	if(a1 == b1) return 0;
+    	return b1 > a1? 1: -1;
+	});	
+
+	console.log(data.length)
+	var uniqueEntities = _.uniq(data, function (item, key, a) {return item.columnA.label;});
+	console.log(uniqueEntities.length)
+
+	lastlabel ='';
+	thislabel ='';
+
+	$.each(uniqueEntities, function( index, value) {
+		thislabel= value.columnA.label;
+		if(thislabel!= lastlabel){
+  			$('#columnA').append('<li id="columnA_'+index+'" qspColA="'+thislabel+'" class="list-group-item">'+
+  		      '<div class="alert " role="alert"> <span class="badge">'+value.columnA.columnCount+'</span>'+
+  		      '<span id="columnA" >'+thislabel+'</span></div></li>');
+  		}
+  		lastlabel = thislabel;
+	});	
+}	
+
+function populateColumnB(data){	
+	stroll.bind( '#datadiv ul' );
+
+	$('#columnB').empty();
+	$.each(data, function( index, value) {
+		var listclass='';
+  		$('#columnB').append('<li id="columnB_'+index+'" qsp="'+value+'" class="list-group-item '+listclass+'">'+
+  		      '<div class="alert " role="alert"><span id="columnB" >'+value.columnB+'</span></div></li>');
+	});	
+}
+
